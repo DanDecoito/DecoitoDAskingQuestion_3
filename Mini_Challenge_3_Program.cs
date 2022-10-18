6127fb5c6ad8f1094d745fc65b161f7b27c6e80d
Dan Decoito <DanielDecoito@gmail.com>
2022-10-18 15:54:00 -0700

    First Commit

---
+++ Mini_Challenge_3_Program.cs
@@ -0,0 +1,31 @@
+﻿// Daniel Decoito 
+// 9-8-2022
+// Mini Challenge 3 "Ask a question"
+// Create a program where you ask the user a question and use thier response in a phrase
+// Peer Reviewed Elizar Garcia. Ran great, worked as intended.
+
+
+Console.Clear();
+
+string userName;
+string wakeTime;
+string terminate = ".";
+
+
+static string QuestionStuff(string name, string time)
+{
+    return $"Hello {name}, you woke up at {time} today";
+}
+
+
+while (terminate != "")
+{Console.WriteLine("Hello, user what is your name?");
+userName = Console.ReadLine();
+Console.WriteLine("And what time did you wake up today " + userName);
+wakeTime = Console.ReadLine();
+Console.WriteLine(QuestionStuff(userName, wakeTime));
+Console.WriteLine("Do you wanna play again? press any key then enter to do so \nIf you want the program to end press enter ");
+Console.WriteLine("");
+terminate = Console.ReadLine();}
+Console.WriteLine("Thanks for playing.");
+
